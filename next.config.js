7714d3cc4bc28efafc593a41fe6243d3a33722b8
Jick Lampago <jick.lampagi@serino.com>
2024-07-31 21:37:07 +0800

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // ... other options you like
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
